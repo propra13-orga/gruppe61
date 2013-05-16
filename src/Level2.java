0ebde45fbd1c78faa5b0c0ca81be164bd73df777
Polles <jan.ippers@uni-duesseldorf.de>
2013-05-16 15:12:23 +0200

    bugs fixed

--- src/Level2.java
+++ src/Level2.java
@@ -118,7 +118,7 @@ public static void main(String[] args) {
                 while (x>=0.001 && x<=1 )
                  
                 {	
-                	StdDraw.show(0);
+                	StdDraw.show(1);
                 	//damit das Feld nach der Falle wieder weiß wird
                 	//Weissmacher(i,j,0,0);
                 	//Weissmacher(k,l,0,0);
