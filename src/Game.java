99db565ee3d3acf949f7c1b6b7fb03e0eb453744
unknown <weronika.zietek@gmail.com>
2013-06-30 22:31:44 +0200

    sound

--- src/Game.java
+++ src/Game.java
@@ -34,6 +34,13 @@ Paint.player(Globals.startx,Globals.starty);
 
 public static void start() throws IOException, InterruptedException, UnsupportedAudioFileException, LineUnavailableException{
 
+	File file = new File("src/level.wav");
+	AudioInputStream stream = AudioSystem.getAudioInputStream(file);
+	AudioFormat format = stream.getFormat();
+	DataLine.Info info = new DataLine.Info(Clip.class, format);
+	Clip clip = (Clip)AudioSystem.getLine(info);
+	clip.open(stream);
+	clip.start();
 	
 	int p;	// Laufvariable damit in GetRoom nicht immer die Fallen und so mehrfach ausgelesen werden und die Koordinaten in Globals verändert werden.
 	p=0;
@@ -169,7 +176,7 @@ public static void start() throws IOException, InterruptedException, Unsupported
         			Globals.health_boss=3;
         			
         			//Prüfe, ob durchgezockt
-        			if(Globals.level>3) Durchgezockt.go();
+        			if(Globals.level>3) { Durchgezockt.go(); clip.stop(); }
         		
         			
         		}
