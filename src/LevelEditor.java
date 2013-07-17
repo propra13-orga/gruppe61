12d77171b462993f3d8ac62143da1ba3cc692f7a
unknown <weronika.zietek@gmail.com>
2013-07-17 06:11:51 +0200

    Karteneditor, teil 1, error

---
+++ src/LevelEditor.java
@@ -0,0 +1,139 @@
+import javax.swing.JFrame; //imports JFrame library
+import javax.swing.JButton; //imports JButton library
+import java.awt.GridLayout; //imports GridLayout library
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+ 
+public class LevelEditor extends JFrame implements ActionListener{
+ 
+        /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+		JFrame frame=new JFrame(); //creates frame
+        static JButton[][] grid; //names the grid of buttons
+        static String [][] karte;
+        static String sig;
+        static String empty = "      ";
+		
+		
+        public LevelEditor(){ //constructor
+                frame.setLayout(new GridLayout(21,21)); //set layout
+                grid=new JButton[21][21]; //allocate the size of grid
+                for(int y=0; y<21; y++){
+                        for(int x=0; x<21; x++){
+                                grid[x][y]=new JButton("      "); //creates new button 
+                                frame.add(grid[x][y]); //adds button to grid
+                                grid[x][y].addActionListener(this); 
+                                grid(x,y);
+                                
+                        }
+                }
+   
+                
+                
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.pack(); //sets appropriate size for frame
+                frame.setVisible(true); //makes frame visible
+
+					
+           }
+        
+        ArrayList<String> liste = new ArrayList<String>();
+
+        public ArrayList<String> liste(){
+        	
+        	for(int j=0; j<21;j++) 
+    			{
+    				for(int i=0;i<21;i++)
+    				{
+        	          liste.add(grid[i][j].getText());
+    				}
+    			}
+        	
+			return liste;
+        	
+        }
+
+		public static String[][] check(int i, int j){
+			
+			//for(int j=0; j<21;j++) 
+			//{
+			//	for(int i=0;i<21;i++)
+			//	{
+					if (grid[i][j].getText() == "x") karte[i][j] ="x";
+					else if (grid[i][j].getText() == "s") karte[i][j] ="s";
+					else if (grid[i][j].getText() == "z") karte[i][j] ="z";
+				    else if (grid[i][j].getText() == "B") karte[i][j] ="B";
+				    else if (grid[i][j].getText() == "NP")karte[i][j] ="NP";
+				    else if (grid[i][j].getText() == "q") karte[i][j] ="q";
+				    else if (grid[i][j].getText() == "empty") karte[i][j] =" ";
+
+			   //  }
+				
+		//	}
+			return karte;
+				
+
+			
+		}
+		
+
+		public static void action(String sign) {
+			// TODO Auto-generated method stub
+			
+			sig = sign;
+			
+		}
+		
+
+		
+		/*public void actionPerformed(ActionEvent e) 
+		{
+			// TODO Auto-generated method stub
+			for(int j=0; j<21;j++) {
+				for(int i=0;i<21;i++){
+					
+					grid(i,j);
+				}
+			}
+		
+			
+		}*/
+		
+		
+		public void grid(final int i, final int j){
+			
+			grid[i][j].addActionListener(
+            	    new ActionListener() {
+            	        public void actionPerformed(ActionEvent e) {
+            	        	if (grid[i][j].getText() == sig)  
+                          		
+                      		{grid[i][j].setText(empty); }
+                        	else 
+                      		{ grid[i][j].setText(sig); }
+            	         	        
+            	        }
+            	    	
+            	    });
+			
+     
+			
+		}
+
+
+
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+
+		
+
+}
---
+++ src/MenuEditor.java
@@ -0,0 +1,369 @@
+import java.awt.Component;
+import java.awt.event.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+
+
+public class MenuEditor extends JPanel {
+
+	
+	 /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	static JFrame frame;
+	    static String wand = new String("Wand");
+	    static String start = new String("Start");
+	    static String ziel = new String("Ziel");
+	    static String boss = new String("Boss");
+	    static String q = new String("Questgegenstand");
+	    static String np = new String("NP");
+	    static String onea = new String("1a");
+	    static String twoa = new String("2a");
+	    static String threea = new String("3a");
+	    static String oneb = new String("1b");
+	    static String twob = new String("3b");
+	    static String threeb = new String("3b");
+	    JButton save, cancel, upload;
+	    static String[][] karte;
+	   
+	    
+	    
+	    public MenuEditor() {
+		super(true);
+		
+
+		save = new JButton("Speichern");
+		cancel = new JButton("Abbrechen");
+		upload = new JButton("Hochladen");
+		
+		// Create the buttons.
+		JRadioButton X = new JRadioButton(wand);
+		X.setActionCommand(wand);
+		X.setSelected(true);
+
+		JRadioButton S = new JRadioButton(start);
+		S.setActionCommand(start);
+		
+		JRadioButton Z = new JRadioButton(ziel);
+		Z.setActionCommand(ziel);
+
+		JRadioButton B = new JRadioButton(boss);
+		B.setActionCommand(boss);
+		
+		JRadioButton Q = new JRadioButton(q);
+		Q.setActionCommand(q);
+		
+		JRadioButton NP = new JRadioButton(np);
+		NP.setActionCommand(np);
+		
+		
+
+		JRadioButton one = new JRadioButton("Level 1");
+		one.setActionCommand(onea);
+		
+		JRadioButton two = new JRadioButton("Level 2");
+		two.setActionCommand(twoa);
+		
+		JRadioButton three = new JRadioButton("Level 3");
+		three.setActionCommand(threea);
+		
+		JRadioButton one1 = new JRadioButton("Raum 1");
+		one1.setActionCommand(oneb);
+		
+		JRadioButton two1 = new JRadioButton("Raum 2");
+		two1.setActionCommand(twob);
+		
+		JRadioButton three1 = new JRadioButton("Raum 3");
+		three1.setActionCommand(threeb);
+
+		// Group the radio buttons.
+		ButtonGroup group = new ButtonGroup();
+		group.add(X);
+		group.add(S);
+		group.add(Z);
+		group.add(B);
+		group.add(Q);
+		group.add(NP);
+		
+		ButtonGroup level= new ButtonGroup();
+		level.add(one);
+		level.add(two);
+		level.add(three);
+
+		ButtonGroup raum = new ButtonGroup();
+		raum.add(one1);
+		raum.add(two1);
+		raum.add(three1);
+	
+
+	        // Register a listener for the radio buttons.
+		RadioListener myListener = new RadioListener();
+		X.addActionListener(myListener);
+		S.addActionListener(myListener);
+		Z.addActionListener(myListener);
+		B.addActionListener(myListener);
+		Q.addActionListener(myListener);
+		NP.addActionListener(myListener);
+		
+		one.addActionListener(myListener);
+		two.addActionListener(myListener);
+		three.addActionListener(myListener);
+		one1.addActionListener(myListener);
+		two1.addActionListener(myListener);
+		three.addActionListener(myListener);
+
+		add(X);
+		add(S);
+		add(Z);
+		add(B);
+		add(Q);
+		add(NP);
+		add(save);
+		add(cancel);
+		add(one);
+		add(two);
+		add(three);
+		add(one1);
+		add(two1);
+		add(three1);
+		
+		
+	    }
+
+
+	    
+	    /** Listens to the radio buttons. */
+	    class RadioListener implements ActionListener { 
+		public void actionPerformed(ActionEvent e) {
+
+			save.addActionListener(
+            	    new ActionListener() {
+            	        public void actionPerformed(ActionEvent e) {
+            	        	
+            	        	//save(1,1);
+            	        	
+            	        	if ((e.getActionCommand() == onea) && (e.getActionCommand() == oneb) )
+            			    {
+            			    	save(1,1);
+            			    } 
+            			    else if  ((e.getActionCommand() == onea) && (e.getActionCommand() == twob) )
+            			    {
+            			    	save(1,2);
+
+            			    }
+            			    else if  ((e.getActionCommand() == onea) && (e.getActionCommand() == threeb) ) 
+            			    {
+            			    	save(1,3);
+
+            			    }
+            			    else if  ((e.getActionCommand() == twoa) && (e.getActionCommand() == oneb) )
+            			    {
+            			    	save(2,1);
+
+            			    } 
+            			    else if  ((e.getActionCommand() == twoa) && (e.getActionCommand() == twob) ) 
+            			    {
+            			    	save(2,2);
+
+            			    }
+            			    else if  ((e.getActionCommand() == twoa) && (e.getActionCommand() == threeb) )
+            			    {
+            			    	save(2,3);
+
+            			    }
+            			    else if  ((e.getActionCommand() == threea) && (e.getActionCommand() == oneb) )
+            			    {
+            			    	save(3,1);
+
+            			    } 
+            			    else if  ((e.getActionCommand() == threea) && (e.getActionCommand() == twob) ) 
+            			    {
+            			    	save(3,2);
+
+            			    }
+            			    else if  ((e.getActionCommand() == threea) && (e.getActionCommand() == threeb) )
+            			    {
+            			    	save(3,3);
+
+            			    }
+   
+
+            	        	
+            	        }
+
+            	    });
+			
+		    cancel.addActionListener(
+            	    new ActionListener() {
+            	        public void actionPerformed(ActionEvent e) {
+            	      
+            	        	System.exit(0);
+            	        }
+
+            	    });
+		    
+		    upload.addActionListener(
+            	    new ActionListener() {
+            	        public void actionPerformed(ActionEvent e) {
+            	      
+            	        	System.exit(0);
+            	        }
+
+            	    });
+		    
+		    if (e.getActionCommand() == start) 
+		    {
+			System.out.println(start + " pressed.");
+			LevelEditor.action("s");
+		    } 
+		    else if (e.getActionCommand() == ziel) 
+		    {
+			System.out.println(ziel + " pressed.");
+			LevelEditor.action("z");
+		    }
+		    else if (e.getActionCommand() == boss) 
+		    {
+				System.out.println(boss + " pressed.");
+				LevelEditor.action("B");
+			}
+		    else if (e.getActionCommand() == q) 
+		    {
+				System.out.println(q + " pressed.");
+				LevelEditor.action("q");
+			}
+		    else if (e.getActionCommand() == np) 
+		    {
+				System.out.println(np + " pressed.");
+				LevelEditor.action("NP");
+			}
+		    else if (e.getActionCommand() == wand) 
+		    {
+				System.out.println(wand + " pressed.");
+				LevelEditor.action("x");
+			}
+
+		}
+		}
+		
+
+		public static String[][] action(String [][] kk) {
+			// TODO Auto-generated method stub
+			
+			return karte = kk;
+			
+		}
+		
+
+		public static void read()
+		{
+			
+
+		}
+		
+		
+	    public void save(int a, int b ){
+	    	
+	    	try {
+
+	    		
+				File file = new File("src/Raum" + a + "-" + b + ".txt");
+	 
+				// if file doesnt exists, then create it
+				if (!file.exists()) {
+					file.createNewFile();
+				}
+	 
+				
+				
+				FileWriter fw = new FileWriter(file.getAbsoluteFile());
+				BufferedWriter bw = new BufferedWriter(fw);
+		
+				
+				System.out.println(LevelEditor.check(a,b));
+				
+				//karte = action(LevelEditor.check(a, b));
+			
+			    for(int j=0; j<21; j++){
+					bw.write(karte[0][j]);
+					bw.write("\n");
+					bw.write(karte[1][j]);
+					bw.write("\n");
+					bw.write(karte[2][j]);
+					bw.write("\n");
+					bw.write(karte[3][j]);
+					bw.write("\n");
+					bw.write(karte[4][j]);
+					bw.write("\n");
+					bw.write(karte[5][j]);
+					bw.write("\n");
+					bw.write(karte[6][j]);
+					bw.write("\n");
+					bw.write(karte[7][j]);
+					bw.write("\n");
+					bw.write(karte[8][j]);
+					bw.write("\n");
+					bw.write(karte[9][j]);
+					bw.write("\n");
+					bw.write(karte[10][j]);
+					bw.write("\n");
+					bw.write(karte[11][j]);
+					bw.write("\n");
+					bw.write(karte[12][j]);
+					bw.write("\n");
+					bw.write(karte[13][j]);
+					bw.write("\n");
+					bw.write(karte[14][j]);
+					bw.write("\n");
+					bw.write(karte[15][j]);
+					bw.write("\n");
+					bw.write(karte[16][j]);
+					bw.write("\n");
+					bw.write(karte[17][j]);
+					bw.write("\n");
+					bw.write(karte[18][j]);
+					bw.write("\n");
+					bw.write(karte[19][j]);
+					bw.write("\n");
+					bw.write(karte[20][j]);
+					bw.write("\n");				
+					bw.close();
+					}
+				
+	 
+				Component frame = null;
+				JOptionPane.showMessageDialog(frame, "Das Spiel wurde erfolgreich gespeichert");
+	 
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	    	
+	    }
+	 
+	    
+	    public static void main(String s[]) {
+	         WindowListener l = new WindowAdapter() {
+	             public void windowClosing(WindowEvent e) {System.exit(0);}
+	         };
+	 
+	         frame = new JFrame("Menü Editor");
+	         frame.addWindowListener(l);
+	         frame.add("Center", new MenuEditor());
+	         frame.pack();
+	         frame.setVisible(true);
+	         
+	         new LevelEditor();
+	         
+	    }
+
+
+
+}
